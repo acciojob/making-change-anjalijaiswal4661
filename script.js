4df208dfd2c7548c29a05916801be78377822dee
anjalijaiswal4661 <161955948+anjalijaiswal4661@users.noreply.github.com>
2026-07-19 18:48:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const makeChange = (c) => {
   // your name here
+	const coins = [
+  ['q', 25],
+  ['d', 10],
+  ['n', 5],
+  ['p', 1]
+];
+let obj={};
+let count=0;
+	forEach(coin of coins){
+		 coinName = coin[0]
+        coinValue = coin[1]
+		count=c/coinValue;
+		obj[coinName]=count;
+		c=c%coinValue;
+	}
+	return obj;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+// const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
